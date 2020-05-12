7da1eb8a4ca00de005a69aa5430ea204c1d74484
hirokiaihara <hiro0818012359@gmail.com>
2020-05-12 20:30:41 +0900

    スクロール検知とアニメーション

---
+++ JavaScriptスクロール※アニメーション/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const cb = function(el, isIntersecting) {
+    if(isIntersecting) {
+      el.classList.add('inview');
+    }
+  }
+  const so = new ScrollObserver('.cover-slide', cb);
+});
