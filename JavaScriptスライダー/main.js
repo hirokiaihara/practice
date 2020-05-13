5d4fc11095f54fd68ee1c54ed01467af6bfea5c5
hirokiaihara <hiro0818012359@gmail.com>
2020-05-13 22:55:18 +0900

    ヒーロースライダー

---
+++ JavaScriptスライダー/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider('.swiper-container');
+  hero.start();
+  // setTimeout(() => {
+  //   hero.stop();
+  // }, 5000);
+});
